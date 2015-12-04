50abace77808a551658d982ad704fd303548bde6
rc369 <rc369@njit.edu>
2015-12-03 21:29:01 -0500

    Create edm-dance.js
    Here is a new js file with my EDM-Dance list

---
+++ edm-dance.js
@@ -0,0 +1,162 @@
+var questions = {"edm-dance":[
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/Ep2WjdZrF78RstnwJGkQJEd55fHLbptWgh3PrAe61oI2RXSSFul6mlvipEzFI6XT/file",
+	"choice1":"Till the sky falls down",
+	"choice2":"Red lights",
+	"choice3":"So Get Up",
+	"choice4":"Secrets",
+	"answer":"Red lights"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/775qzEUdy1H4DOnyQR2qEGW4BCQL9nWdZGe8F0A5wrJqX1tq6p7m8o9OLoqswoFp/file",
+	"choice1":"Red lights",
+	"choice2":"Rosaires",
+	"choice3":"Not Alone",
+	"choice4":"Till the sky falls down",
+	"answer":"Till the sky falls down"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/4n93QrTxaaUvcolBYrwLvhToDqPJWzItNpJxWvxfhpsu0AfECHrNq2bhyZiNSDCJ/file",
+	"choice1":"So Get Up",
+	"choice2":"Till the sky falls down",
+	"choice3":"Trouble man",
+	"choice4":"Spark",
+	"answer":"So Get Up"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/xHK40LdvxcaIkv5RateuXOG12UKuBSCLRcNHgmuB3kvsvpRKpBiZLVXdKDyQyFxy/file",
+	"choice1":"City of lights",
+	"choice2":"Turn it up",
+	"choice3":"Not Alone",
+	"choice4":"One day at a time",
+	"answer":"Not Alone"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/blRMlwxsSK9uhBUIRCbiMdAKBToqrTkiNvzkCbXs3y2kFzcnafOofg0fTJHZw60n/file",
+	"choice1":"Rosaires",
+	"choice2":"Pyramid",
+	"choice3":"Cross",
+	"choice4":"Bump",
+	"answer":"Rosaires"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/ZTwZCcGRsfebt6gN1oXtlQgUbjTPuNMKE9ddBm75TBVopVALaNQoTja0FmluJF9S/file",
+	"choice1":"City of lights",
+	"choice2":"Hooka",
+	"choice3":"Secrets",
+	"choice4":"West key",
+	"answer":"Secrets"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/XDxqGQtq7X5pAB4ITEUTrTUZF73SFuNvFNFyB3FeoskD63cSpmJjEY40FDdcnl4G/file",
+	"choice1":"Wizard",
+	"choice2":"Tsunami",
+	"choice3":"Born to rage",
+	"choice4":"One Day At A Time",
+	"answer":"One Day At A Time"
+	},
+    {
+	"song":"https://dl.dropboxusercontent.com/content_link/oWnRgmHYehJR5YHcbar7VtueGxICe7MmpArOVG4nwq3YLjRUUT0OSLF9VhRCzECF/file",
+	"choice1":"Dangerous",
+	"choice2":"Who's That Chick?",
+	"choice3":"Turn it up",
+	"choice4":"Just Dance",
+	"answer":"Who's That Chick?"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/faBFiIKvcp3RU4HpffuPAb4aqTs0RE8Qw4bt5AmXReFNCkwqFI8JAxhoPIfuVESX/file",
+	"choice1":"Adagio For Strings",
+	"choice2":"Supreme",
+	"choice3":"Blueprint",
+	"choice4":"Revolution",
+	"answer":"Adagio For Strings"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/XNWozTV1ossiU07vwBHTGpZxSKiajS1T8tcikh3fFWzYssJ0rD9xNxhQukbBlQuK/file",
+	"choice1":"Think I'm",
+	"choice2":"Don't you do it",
+	"choice3":"Careless",
+	"choice4":"Rocker",
+	"answer":"Careless"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/bzM1oFajLzvqKagMVjs9Op91rlSpUwgHddfEt4l4xDyMzutKA4B1qKaQU5gxknFU/file",
+	"choice1":"Twisted",
+	"choice2":"Lifted",
+	"choice3":"Revolutions",
+	"choice4":"Sugar",
+	"answer":"Sugar"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/2UWWCyM5bNrPjdUC0FC0KC0mMG3Xys8UbsDonni0aZ2cUi49Yjdnt38Pq2nTIgji/file",
+	"choice1":"Dangerous",
+	"choice2":"Do it again",
+	"choice3":"Spin me around",
+	"choice4":"Sugar",
+	"answer":"Dangerous"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/G4ZiueQgMAaXGzrgE9LHawx5aUPdEbvnFrdpzopGOgjIDTQM18ClPwrBcL4G6ZnO/file",
+	"choice1":"I give up",
+	"choice2":"Surrender",
+	"choice3":"Love it",
+	"choice4":"Flute",
+	"answer":"Surrender"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/2clK0tQEN9aOK2Jce9rDBYaGoEkI4efb7eCAUs2CNvOYQwIXaKj0qOtY6fCeNZU8/file",
+	"choice1":"Jumper",
+	"choice2":"WickIt",
+	"choice3":"Keep it coming",
+	"choice4":"My Roots",
+	"answer":"My Roots"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/mqOj5OdhgDUDVqLxWOakQ9hFwcbDhfNRkoUyAJT76po2satUBg9ZKKBRUipAhGja/file",
+	"choice1":"Dance all night",
+	"choice2":"Booyah",
+	"choice3":"Revolution",
+	"choice4":"Don't give up",
+	"answer":"Booyah"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/UzDvQIH9VJylYK9bNs5GGl3UFmkZDNjd5vGakJDNwUpdAHEAcC7O76kEXgeVP6so/file",
+	"choice1":"Hey hey",
+	"choice2":"Ordinary day",
+	"choice3":"I heard you say",
+	"choice4":"Bump it",
+	"answer":"Hey hey"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/dOjn3iBpeB5uctFTFc5tJWDo2a6QHUTVHMXppoho0miujaHuel2SzksXUs4DRhM6/file",
+	"choice1":"It's time",
+	"choice2":"Take control",
+	"choice3":"Turn on the music",
+	"choice4":"Lifestyle",
+	"answer":"Turn on the music"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/PHF8O4iUnTpikXGnEhLKwJU7zSIVorvyTz5W7dpAuqKkF3OOruXHMzD6f6z2H0i1/file",
+	"choice1":"Titanium",
+	"choice2":"Project T",
+	"choice3":"Faith",
+	"choice4":"Shot me down",
+	"answer":"Titanium"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/p6OQBJYEFY4w2X3ZcBX3fCqVzCxyv1cZupxbDLQAkAUw3jb69EY1IFtna4zmA06r/file",
+	"choice1":"Creepin",
+	"choice2":"South Keys",
+	"choice3":"Miami beats",
+	"choice4":"How we do",
+	"answer":"South Keys"
+	},
+  {
+	"song":"https://dl.dropboxusercontent.com/content_link/Jmq52l49cn81PN3RkrSofZWUdG3ozQB8p6qOZd3TtL6uj04Aorlq5kcM7657orYh/file",
+	"choice1":"Summertime Sadness",
+	"choice2":"Red dress",
+	"choice3":"Who's that chick?",
+	"choice4":"Raise it high",
+	"answer":"Summertime Sadness"
+	}
+]};
