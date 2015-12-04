cd570cc614c0dc723cb6d8e0d656e4814b447790
Yangfan <syhhsyhk13@gmail.com>
2015-12-03 20:59:39 -0500

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,1 +1,378 @@
-
+"hip_hop R&B":[
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/D29LdD1EAYaLSwrBATqefqNO9qH0Gl4X2sbS9DYCGBZuk7nWjT6SeeZcYkgWrNBP/file",
+	"choice1":"Diamonds Dancing",
+	"choice2":"Plastic Bag",
+	"choice3":"Jersey",
+	"choice4":"Digital Dash",
+	"answer":"Digital Dash"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/x3w1fvOcXKGb3pdpaKW9ibjSxtBErXsesbBtbeXpEYDb0Nl9LV7KmB0vQA612tPH/file",
+	"choice1":"Single Ladies",
+	"choice2":"Drunk in Love",
+	"choice3":"Crazy in Love",
+	"choice4":"Partition",
+	"answer":"Crazy in Love"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/a8gHXPdF1ylT5veH9MxUqSM3yiUmVoazwjMMy8tZudZgnECnK9NUNaPTAbUcclBQ/file" ,
+	"choice1":"One in a Million",
+	"choice2":"Are You That Somebody",
+	"choice3":"Try Again",
+	"choice4":"I Don't Wanna",
+	"answer":"One in a Million"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/ou2wWjVoGzJgYMmcn7wrN0VsqwfkkLdq52xyB739Mz21trRJBkcUdttxrKWatcxJ/file" ,
+	"choice1":"I Miss You",
+	"choice2":"More Than a Woman",
+	"choice3":"Rock the Boat",
+	"choice4":"Hot Like Fire",
+	"answer":"Rock the Boat"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/OfXpOaG3wMvGO7Z1xYltChIs6jUqSsIaT1LFk2vo7itAWiFdKEkPlwkbdDL5hzyG/file" ,
+	"choice1":"Jumpman",
+	"choice2":"Big Rings",
+	"choice3":"30 for 30",
+	"choice4":"I'm the Plug",
+	"answer":"Big Rings"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/71CzJfH8cuyluAyq6g0gexh2YiXVwDDq4Tf3gnfJl3Hs9CiARJPdx4fDZV2BZgfW/file" ,
+	"choice1":"Run the World",
+	"choice2":"XO",
+	"choice3":"Love on Top",
+	"choice4":"Naughty Girl",
+	"answer":"Naughty Girl"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/DtYW38Sd9sT1sFiNsnWDGIdss404oBDlLN4vkYTa8I43gSNaJVrbzkrONan7vV8E/file" ,
+	"choice1":"How to Love",
+	"choice2":"A Milli",
+	"choice3":"Mirror",
+	"choice4":"Lollipop",
+	"answer":"A Milli"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/tJj4izJSYEgnV1Ia54OtVRF3L2eX3Y6hBIQXyvb9akNOxWuaIeYW03H7ggqcquWY/file" ,
+	"choice1":"Say My Name",
+	"choice2":"Survivor",
+	"choice3":"Cater 2 U",
+	"choice4":"Lose My Breath",
+	"answer":"Cater 2 U"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/8558yv4kwPmPl1QVamSCtbp9c8EgQKZ8eSqQHqcT6LyVRsbxRrhTb4urtS7SHWXj/file" ,
+	"choice1":"Gin and Juice",
+	"choice2":"Who Am I",
+	"choice3":"Drop It Like It's Hot",
+	"choice4":"California Roll",
+	"answer":"Drop It Like It's Hot"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/ZzWOlH3AEQTCiPotOOE7yyy7TwQeovzpnqJrkLNOiUl5hgDfd7ins2LlW2qLIDdB/file" ,
+	"choice1":"Diamonds Dancing",
+	"choice2":"Where Ya At",
+	"choice3":"Move That Dope",
+	"choice4":"Live From The Gutter",
+	"answer":"Where Ya At"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/0pJM1mW8ZoVFoxLeI4dvMKKtFfMmTyZwygiSiNdmSRbeN2md1ax5ygA46I6yjjHX/file" ,
+	"choice1":"Coffee",
+	"choice2":"All I Want Is You",
+	"choice3":"Adorn",
+	"choice4":"How Many Drinks?",
+	"answer":"Coffee"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/uk2Z5kQ1IX8L6MQvUXmTtVJwmZuGnBrreQGqjSawboqTzjPxEad80vR9vPOEGXQ2/file" ,
+	"choice1":"Stronger",
+	"choice2":"Flashing Lights",
+	"choice3":"Gold Digger",
+	"choice4":"Blood on the Leaves",
+	"answer":"Gold Digger"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/bvbkP5llRDm7hIHl5T8Udl6MLCCkYbkBKQ6BQcyXyZCx6976sdiMsVbpFQuEP0eC/file" ,
+	"choice1":"Get Ur Freak On",
+	"choice2":"Lose Control",
+	"choice3":"We Run This",
+	"choice4":"Work It",
+	"answer":"Work It"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/b9gsRzuCBn0OPswlBR6jUQ2vl7r34391odAsG2Anx9bvNPmg1tSoTJEfOoSrnmm3/file" ,
+	"choice1":"All of the Lights",
+	"choice2":"Jesus Walks",
+	"choice3":"All Day",
+	"choice4":"Touch the Sky",
+	"answer":"All of the Lights"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/TEmqhFtsyG6t0AEP4MZ7ZFaQ4Q3ExLAnlc0kZIYUfdbOd9fOE8YccmQotzKM8Cof/file" ,
+	"choice1":"Blessings",
+	"choice2":"Play No Games",
+	"choice3":"My Last",
+	"choice4":"Paradise",
+	"answer":"Play No Games"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/KLSjZRY5FV6HpW4ikwE0k6c0vJoV2LKe5i75FbrloI2BawSQoVmMi2qsnGpGmnhc/file" ,
+	"choice1":"Marvins Room",
+	"choice2":"Headlines",
+	"choice3":"Take Care",
+	"choice4":"Best I Ever Had",
+	"answer":"Take Care"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/rjaqTCpyoVbbhFCVfwHKEuiDYruXmr8KSRTWMhk1k0h0gdhZyVohPhoPLh2Mw7ST/file" ,
+	"choice1":"Wicked Games",
+	"choice2":"Dark Times",
+	"choice3":"The Hills",
+	"choice4":"Can't Feel My Face",
+	"answer":"The Hills"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/ZI0H80TQRm0PVyvksfIRGlqZ6bOkkJnqvLpP5FWyz0a8KAAuIhByIFRvur8LQ4hF/file" ,
+	"choice1":"Dance",
+	"choice2":"Paradise",
+	"choice3":"Research",
+	"choice4":"Clique",
+	"answer":"Paradise"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/27KSKgeqg0ItPy1lpEDTht8fj4ZcGwfym9CcOYjDVopNxVlxqceV3kKu8rZvuK71/file" ,
+	"choice1":"Flawless",
+	"choice2":"Halo",
+	"choice3":"Partition",
+	"choice4":"End of Time",
+	"answer":"Partition"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/vCk8slfqeO9UeyV90NgkMO21jhd4EWavg14WC6hrgciWhgy6Qq1692DO3YGMHkjr/file" ,
+	"choice1":"Worst Behavior",
+	"choice2":"Used To",
+	"choice3":"HYFR",
+	"choice4":"All Me",
+	"answer":"Worst Behavior"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/f2mlbLa6tGRSEFzumnK3kpqjS1xrDc85GwhfSra1JnFn23EjeBWK1vXxCJ0w1jYy/file" ,
+	"choice1":"Often",
+	"choice2":"Can't Feel My Face",
+	"choice3":"Tell Your Friends",
+	"choice4":"Dark Times",
+	"answer":"Can't Feel My Face"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/fHsmN2fAwZrhac8cRlnFbx3rpb1890ryYrSapgVkcbAdr8hGRBAmjb6K5vOtm7mQ/file" ,
+	"choice1":"Hold On, We're Going Home",
+	"choice2":"Started From the Bottom",
+	"choice3":"6 God",
+	"choice4":"Energy",
+	"answer":"Hold On, We're Going Home"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/e81PISfqivKWaGyUHj4z29rC6Ssy0vZrz2e9JJHc0vWvioYzISAGFkGrJja26MeG/file" ,
+	"choice1":"Irreplaceable",
+	"choice2":"Pretty Hurts",
+	"choice3":"Rocket",
+	"choice4":"Beautiful Liar",
+	"answer":"Rocket"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/RMuZhqWWnNYWaBo9kAXYxraIBuVnOSCev3AtVo1Rdc1dPvnFUpLc6sJZxzzrfFQ9/file" ,
+	"choice1":"Maria I'm Drunk",
+	"choice2":"Never Catch Me",
+	"choice3":"Antidote",
+	"choice4":"Apple Pie",
+	"answer":"Antidote"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/csms99IwpfW2VsYkmW0ITYqhRPDZXzEc6ykJGRW3G1TNlRz57w5I17tPliwppkAH/file" ,
+	"choice1":"Live For",
+	"choice2":"In The Night",
+	"choice3":"Earned It",
+	"choice4":"Loft Music",
+	"answer":"Earned It"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/IRACHnNlMb2Lbkqg9KwLxkQ7298FbVMWmKcpOaAQtc2ZIlkpK0tijom8yWiBftOC/file" ,
+	"choice1":"Research",
+	"choice2":"I Know",
+	"choice3":"Deserve It",
+	"choice4":"Dark Sky",
+	"answer":"I Know"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/X8o7eQwYNasGflmI1kmF6PA9HFNkNq2HklpZKSmLUeZiGGanliOdFvSE0GLRwkWH/file" ,
+	"choice1":"Jumpman",
+	"choice2":"Jersey",
+	"choice3":"Where Ya At",
+	"choice4":"Big Rings",
+	"answer":"Jumpman"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/zQPaZXgTDLwcOpn8aTV4Dr4ImmvJ5UO3jjrLqvbeUkHwGmcYXgBN6rVoD3pv3pXS/file" ,
+	"choice1":"Thought It Was A Drought",
+	"choice2":"I'm The Plug",
+	"choice3":"Karate Chop",
+	"choice4":"Jersey",
+	"answer":"Jersey"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/qxYPXfq2SN9ekEXHdelHP1H2UqRPRlRplT4TeNghaKRJYOK6ltmkxUhfUD13yOqU/file" ,
+	"choice1":"Grown Woman",
+	"choice2":"Ego",
+	"choice3":"Flawless",
+	"choice4":"Partition",
+	"answer":"Flawless"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/JCJAhyZcdFBuOscnYsQqnPru493LOerDQUcB5okoEsSXwyDst8S7j6PoUI5SUhEb/file" ,
+	"choice1":"Beware",
+	"choice2":"Research",
+	"choice3":"Play No Games",
+	"choice4":"I Don't Like",
+	"answer":"Research"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/E9xhkzcTX8QtLcU9Z3LdOXhdRLh8LNfrP0gsGIRb3fnnMDKWOivN6vDrcVAqMe6i/file" ,
+	"choice1":"The Motto",
+	"choice2":"How About Now",
+	"choice3":"HYFR",
+	"choice4":"Crew Love",
+	"answer":"HYFR"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/ClLpM4dzsQRsmhGADZt703YmyXzUxSAdLE1FbobAAZkIxnCtvTp6PhHz5mnHxUc5/file" ,
+	"choice1":"Loyal",
+	"choice2":"Drunk Texting",
+	"choice3":"Fine China",
+	"choice4":"Love More",
+	"answer":"Drunk Texting"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/QBLY3N8STDg9XXfYNghDYijIm3RO05OX6WGbbRKi52tQNkNhXuqmDxRpf104IjoB/file" ,
+	"choice1":"R.I.C.O.",
+	"choice2":"All Eyes On You",
+	"choice3":"Levels",
+	"choice4":"Amen",
+	"answer":"All Eyes On You"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/MBZ5SjEWemZoIaN4JOnSZrmweDlWObegNhlaw25bh1PgzRRbjdwWf8knBe7bZosQ/file" ,
+	"choice1":"Hotline Bling",
+	"choice2":"0 To 100",
+	"choice3":"Back To Back",
+	"choice4":"Worst Behavior",
+	"answer":"Back To Back"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/6ULdxQ2YvrDa7ptvvvlHCweCxO9B5hZz5EzfYIg9cdx47v5WPxX5bEm4j2f2egos/file" ,
+	"choice1":"End of Time",
+	"choice2":"I Was Here",
+	"choice3":"7/11",
+	"choice4":"Get Me Bodied",
+	"answer":"7/11"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/ITptDn88YN5sOFbzUeLNkB11dDdM5BjRyQZxWpn8SVZ24LRqAueqzYjNJvj1eAsu/file" ,
+	"choice1":"Don't",
+	"choice2":"Rambo",
+	"choice3":"Let Em' Know",
+	"choice4":"Sorry Not Sorry",
+	"answer":"Don't"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/DTT4qNeAha6WfEdLe1sFW0O9OEZyNEUhtGWkVAnDmxbMBuYl7RwyST0ADAgeM9D8/file" ,
+	"choice1":"502 Come Up",
+	"choice2":"Don't",
+	"choice3":"Right My Wrongs",
+	"choice4":"For However Long",
+	"answer":"Right My Wrongs"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/WopUX7jAhnfnJoh1TPlJknIZWW6ynxYBwIf9TIRCDXkX6rDWP35Yke668yOClTg1/file" ,
+	"choice1":"Liquor",
+	"choice2":"Don't Judge Me",
+	"choice3":"Say Goodbye",
+	"choice4":"Deuces",
+	"answer":"Liquor"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/SRMcrNxhwMF8bFA8FoMLS2BLK324atkTxn28OhytS8t6NaBbgpXDy4DtZmf0AC83/file" ,
+	"choice1":"Know Yourself",
+	"choice2":"Best I Ever Had",
+	"choice3":"Doing It Wrong",
+	"choice4":"Find Your Love",
+	"answer":"Find Your Love"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/eOEifnro443QB51tgqqKF7sqJsFZIryvhb2HhO67z27cTwtAc2mecuFSKvkvNPfh/file" ,
+	"choice1":"My Name Is",
+	"choice2":"The Real Slim Shady",
+	"choice3":"Kings Never Die",
+	"choice4":"The Way I Am",
+	"answer":"The Real Slim Shady"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/uyn6iL2ClOS6uCKM4imnpgcc11LPmjofZjIY80qmtgUmWhoKrXqG7w5Rt8BhFT8d/file" ,
+	"choice1":"The Monster",
+	"choice2":"Love the Way You Lie",
+	"choice3":"Lose Yourself",
+	"choice4":"Not Afraid",
+	"answer":"Love the Way You Lie"
+	},	
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/vIptV3Oz0H9XSnq0HBxwJwOE0t7c619umomtbpGApx0myORBzsc9crzImkVXAXrA/file" ,
+	"choice1":"You Be Killin Em",
+	"choice2":"Make Me Better",
+	"choice3":"Can't Let You Go",
+	"choice4":"Baby Don't Go",
+	"answer":"Make Me Better"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/mPwWPl82nqo2M624nx85O1AgZVf0Zfzo9v8vPdo4uez9VHWoBJ5gi7zqjliXyJpN/file" ,
+	"choice1":"Ride Wit Me",
+	"choice2":"Just a Dream",
+	"choice3":"Country Grammar",
+	"choice4":"Hot In Herre",
+	"answer":"Hot In Herre"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/asoieLIYTVXvBUIFEDtCbgeruNBBK1SqHHSjXAFwBsol7ADuGvFREpRClS50WaMm/file" ,
+	"choice1":"Bump n' Grind",
+	"choice2":"I Believe I Can Fly",
+	"choice3":"Ignition",
+	"choice4":"You Remind Me Of Something",
+	"answer":"Ignition"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/rs87Y7oDalwR4RHrJKasTbd4hXpBDQaE1C0RlbP9Ae9Xds4O5AleoWiRuiW9IV96/file" ,
+	"choice1":"Into You",
+	"choice2":"Can't Deny It",
+	"choice3":"Trade It All",
+	"choice4":"Cinnamon Apple",
+	"answer":"Into You"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/vG5g1KrdemvV3hrA18glKtmyAHsaZ3sOWaeGVXrcjrlseHdPrgoMrSXNeKcuGvjU/file" ,
+	"choice1":"Always On Time",
+	"choice2":"The Woman You Love",
+	"choice3":"Don't Leave Me Alone",
+	"choice4":"Where I Stand",
+	"answer":"Always On Time"
+	},
+	{
+	"song":"https://dl.dropboxusercontent.com/content_link/GoF17yzjdNxMzDE1EyCSCbykVwDQzWYdmG1rOpQbBPDRPXwKaokqkbvQKz4LIQuq/file" ,
+	"choice1":"Diamonds",
+	"choice2":"What's My Name",
+	"choice3":"Pour It Up",
+	"choice4":"We Found Love",
+	"answer":"Pour It Up"
+	},	
+]
